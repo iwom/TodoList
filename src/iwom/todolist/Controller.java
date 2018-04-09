53c630c08905228471ba1ea169990049e2f56bd9
ElitePumpkin <iwo.malyszka@gmail.com>
2018-04-09 14:25:39 +0200

    Added Item View

--- src/iwom/todolist/Controller.java
+++ src/iwom/todolist/Controller.java
@@ -1,5 +1,9 @@
 package iwom.todolist;
 import iwom.todolist.datamodel.ToDoItem;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextArea;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -7,6 +11,12 @@ import java.util.List;
 
 public class Controller {
     private List<ToDoItem> toDoItemList;
+
+    @FXML
+    private ListView<ToDoItem> toDoListView;
+
+    @FXML
+    private TextArea toDoItemView;
 
     public void initialize() {
         ToDoItem item1 = new ToDoItem("Mail John", "Tell him to do so", LocalDate.of(2018, 4, 15));
@@ -19,5 +29,16 @@ public class Controller {
         toDoItemList.add(item2);
         toDoItemList.add(item3);
         toDoItemList.add(item4);
+        toDoListView.getItems().setAll(toDoItemList);
+        toDoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
     }
-}
+    @FXML
+    public void handleClickListView() {
+        ToDoItem item = toDoListView.getSelectionModel().getSelectedItem();
+        StringBuilder sb = new StringBuilder(item.getDetails());
+        sb.append("\n\n\n\n");
+        sb.append("Due: ");
+        sb.append(item.getDeadline().toString());
+        toDoItemView.setText(sb.toString());
+    }
+ }
--- src/iwom/todolist/datamodel/ToDoItem.java
+++ src/iwom/todolist/datamodel/ToDoItem.java
@@ -36,4 +36,9 @@ public class ToDoItem {
     public void setDeadline(LocalDate deadline) {
         this.deadline = deadline;
     }
+
+    @Override
+    public String toString() {
+        return shortDescription;
+    }
 }
